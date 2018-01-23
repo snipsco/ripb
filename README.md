3d5491925c8a847584afbd84b23195944168baed
Thibaut Lorrain <fredszaq@gmail.com>
2018-01-23 15:22:53 +0100

    add some documentation and a README

---
+++ README.md
@@ -0,0 +1,19 @@
+# `ripb`
+
+A rust crate providing an implementation of a lock-free type-safe
+in-process bus.
+
+## Guarantees
+
+- In order delivery : messages published on the bus are received in the
+same order by a given subscriber. This is not guaranteed across multiple
+subscribers. IE if you send Messages m1 and m2 on a bus with subscribers
+s1 and s2, both s1 and s2 will receive the messages in the order m1, m2,
+ but s1 may receive m2 before s2 has received m1.
+
+
+## Implementation
+
+Current implementation uses `channel`s, a `thread` to run the bus, and a
+`thread` per subscriber. `Any` and `TypeId` are used to to be able to
+expose a type-safe api
--- src/lib.rs
+++ src/lib.rs
@@ -1,84 +1,100 @@
+//! This crates provides an implementation of a lock-free, type-safe, in-process bus.
+//!
+//! # Guarantees
+//!
+//! - In order delivery : messages published on the bus are received in the same order by a given
+//! subscriber. This is not guaranteed across multiple subscribers. IE if you send Messages m1 and
+//! m2 on a bus with subscribers s1 and s2, both s1 and s2 will receive the messages in the order
+//! m1, m2, but s1 may receive m2 before s2 has received m1.
+//!
+//!
+//! # Implementation
+//!
+//! Current implementation uses [`channel`]s, a [`thread`] to run the bus, and a [`thread`] per
+//! subscriber. [`Any`] and [`TypeId`] are used to to be able to expose a type-safe api
+
 use std::any::{Any, TypeId};
 use std::collections::HashMap;
 use std::sync::Arc;
 use std::sync::mpsc::{Sender, Receiver, channel};
 use std::thread;
 
+
+/// An in process bus.
+///
+/// You can create a bus with the [`new`] method, then create new [`Subscriber`]s with the
+/// [`create_subscriber`] method, and push some messages on the bus with the [`publish`] method.
+/// If you need to send messages from multiple threads, [`clone`] the bus and use one instance per thread
 #[derive(Clone)]
 pub struct Bus {
     tx: Sender<BusTask>,
 }
 
+/// A subscriber to a [`Bus`].
+///
+/// Subscribers are created with the method [`Bus.create_subscriber`]
+///
+/// Register new callbacks with the [`on_message`] method
+pub struct Subscriber {
+    tx: Sender<SubscriberTask>
+}
+
 enum BusTask {
     Publish {
         type_id: TypeId,
-        message: Box<Any + Send>,
-        cloner: Cloner,
+        message: Arc<BoxedMessage>,
         worker: Worker,
     },
     RegisterSubscriber { subscriber: Sender<SubscriberTask> },
 }
 
-pub struct Subscriber {
-    tx: Sender<SubscriberTask>
-}
+/// A message on must be [`Send`] and [`Sync`] to be sent on the bus.
+pub trait Message: Send + Sync {}
+
+impl<T: Send + Sync> Message for T {}
+
+type BoxedMessage = Box<Any + Send + Sync>;
+type BoxedCallback = Box<Any + Send>;
+
 
 enum SubscriberTask {
     Receive {
         type_id: TypeId,
-        message: Box<Any + Send>,
+        message: Arc<BoxedMessage>,
         worker: Arc<Worker>,
     },
     RegisterCallback {
         type_id: TypeId,
-        callback: Box<Any + Send>,
+        callback: BoxedCallback,
     },
 }
 
-struct Cloner {
-    callback: Box<Fn(&Box<Any + Send>) -> (Box<Any + Send>) + Send>
-}
-
-impl Cloner {
-    pub fn of<T: Send + Sync + 'static>() -> Self {
-        Self {
-            // TODO error handler remove this unwrap
-            callback: Box::new(|payload| Box::new(Arc::clone(payload.downcast_ref::<Arc<T>>().unwrap())) as Box<Any + Send>)
-        }
-    }
-
-    pub fn call(&self, to_clone: &Box<Any + Send>) -> Box<Any + Send> {
-        (self.callback)(to_clone)
-    }
-}
-
 struct Worker {
-    // fn (callback, payload)
-    worker: Box<Fn(Arc<Box<Any>>, Box<Any>) + Send + Sync>
+    worker: Box<Fn(Arc<BoxedCallback>, Arc<BoxedMessage>) + Send + Sync>
 }
 
 impl Worker {
-    pub fn of<T: Send + Sync + 'static>() -> Self {
+    pub fn of<T: Message + 'static>() -> Self {
         Self {
-            worker: Box::new(|c: Arc<Box<Any>>, payload: Box<Any>| {
+            worker: Box::new(|c: Arc<BoxedCallback>, payload: Arc<BoxedMessage>| {
                 let callback = c.downcast_ref::<Callback<T>>().unwrap();
-                let message = payload.downcast_ref::<Arc<T>>().unwrap();
+                let message = Any::downcast_ref::<T>(&**payload).unwrap();
                 callback.call(message);
             })
         }
     }
 
 
-    pub fn call(&self, callback: Arc<Box<Any>>, payload: Box<Any>) {
+    pub fn call(&self, callback: Arc<BoxedCallback>, payload: Arc<BoxedMessage>) {
         (self.worker)(callback, payload)
     }
 }
 
-struct Callback<M: Send + Sync + 'static> {
+struct Callback<M: Message> {
     callback: Box<Fn(&M) -> () + Send>
 }
 
-impl<M: Send + Sync + 'static> Callback<M> {
+impl<M: Message> Callback<M> {
     pub fn new<F: 'static>(handler: F) -> Callback<M> where F: Fn(&M) -> () + Send {
         Callback { callback: Box::new(handler) }
     }
@@ -88,6 +104,7 @@ impl<M: Send + Sync + 'static> Callback<M> {
 
 
 impl Bus {
+    /// Create a new bus
     pub fn new() -> Self {
         let (tx, rx) = channel();
 
@@ -95,12 +112,12 @@ impl Bus {
             let mut subscribers: Vec<Sender<SubscriberTask>> = vec![];
             loop {
                 match rx.recv() {
-                    Ok(BusTask::Publish { type_id, message, cloner, worker }) => {
+                    Ok(BusTask::Publish { type_id, message, worker }) => {
                         let callback = Arc::new(worker);
                         for subscriber in subscribers.iter() {
                             subscriber.send(SubscriberTask::Receive {
                                 type_id,
-                                message: cloner.call(&message),
+                                message: Arc::clone(&message),
                                 worker: Arc::clone(&callback),
                             }).unwrap() // TODO what should we do if this fails ? remove the subscriber ?
                         }
@@ -116,17 +133,18 @@ impl Bus {
         Self { tx }
     }
 
+    /// Create a new subscriber for this bus
     pub fn create_subscriber(&self) -> Subscriber {
         let subscriber = Subscriber::new();
         self.tx.send(BusTask::RegisterSubscriber { subscriber: subscriber.tx.clone() }).unwrap(); // TODO error handling
         subscriber
     }
 
-    pub fn publish<M: Send + Sync + 'static>(&self, message: M) {
+    /// Publish a new message on this bus
+    pub fn publish<M: Message + 'static>(&self, message: M) {
         self.tx.send(BusTask::Publish {
             type_id: TypeId::of::<M>(),
-            message: Box::new(Arc::new(message)),
-            cloner: Cloner::of::<M>(),
+            message: Arc::new(Box::new(message)),
             worker: Worker::of::<M>(),
         }).unwrap(); //TODO error handling remove this unwrap()
     }
@@ -145,14 +163,14 @@ impl Subscriber {
 
     fn start(&self, rx: Receiver<SubscriberTask>) {
         thread::spawn(move || {
-            let mut callbacks: HashMap<TypeId, Arc<Box<Any>>> = HashMap::new();
+            let mut callbacks: HashMap<TypeId, Arc<BoxedCallback>> = HashMap::new();
             loop {
                 match rx.recv() {
                     Ok(SubscriberTask::RegisterCallback { type_id, callback }) => {
                         callbacks.insert(type_id, Arc::new(callback));
                     }
                     Ok(SubscriberTask::Receive { type_id, message, worker }) => {
-                        callbacks.get(&type_id).map(|it| worker.call(Arc::clone(it), message));
+                        callbacks.get(&type_id).map(|it| worker.call(Arc::clone(it), Arc::clone(&message)));
                     }
                     /* sender is dead */
                     Err(_) => break
@@ -161,7 +179,9 @@ impl Subscriber {
         });
     }
 
-    pub fn on_message<F, M>(&mut self, callback: F) where F: Fn(&M) + Send + 'static, M: Send + Sync + 'static {
+    /// Register a new callback to be called each time a message of the given type is published on
+    /// the bus
+    pub fn on_message<F, M>(&mut self, callback: F) where F: Fn(&M) + Send + 'static, M: Message + 'static {
 
         // TODO remove this unwrap
         self.tx.send(SubscriberTask::RegisterCallback {
